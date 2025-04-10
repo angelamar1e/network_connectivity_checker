437ce7f843a6a43464744070b9e76f223d558b1a
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-10 17:53:52 +0800

    created network status info class

---
+++ lib/models.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NetworkStatusInfo {
+  final IconData icon;
+  final String description;
+
+  NetworkStatusInfo({required this.icon, required this.description});
+}
