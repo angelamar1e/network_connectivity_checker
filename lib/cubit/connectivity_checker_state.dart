bd9ef95fc783cb181f7f85aae45539a1f1a9d127
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-10 14:28:07 +0800

    added network, internet status enums

--- lib/cubit/connectivity_checker_state.dart
+++ lib/cubit/connectivity_checker_state.dart
@@ -1,5 +1,14 @@
 part of 'connectivity_checker_cubit.dart';
 
+enum NetworkStatus {
+  initial,
+  connectedToWifi,
+  connectedToMobileData,
+  noConnection,
+}
+
+enum InternetConnectivityStatus { internetAccessAvailable, noInternetAccess }
+
 @immutable
 sealed class ConnectivityCheckerState {}
 
